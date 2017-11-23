67c164f0cc73843fa01a3225b9c4943c09012fe8
defasdfasdf <l2568588@mvrht.net>
2017-11-23 13:25:50 +0100

    Progressbar #5

--- js/script.js
+++ js/script.js
@@ -49,7 +49,10 @@ loadJSON(function(responseText){
 function roadmap(){
   information.forEach(function(element){
     document.querySelector("#" + element.for).addEventListener('mdl-componentupgraded', function() {
-    this.MaterialProgress.setProgress(0).setProgress(element.progress);
+    this.MaterialProgress.setProgress(0);
+    setInterval(function(){
+      this.MaterialProgress.setProgress(element.progress);
+    }, 500);
 
   });
 });
