8af85032e2c19f944d064772b7c11fb70f75d29a
defasdfasdf <l2568588@mvrht.net>
2017-11-24 16:51:52 +0100

    Update header

--- js/scripts.js
+++ js/scripts.js
@@ -299,8 +299,7 @@ $(document).ready(function() {
 				$('section.navigation').addClass('fixed mdl-shadow--4dp');
 				$('section.fixed').addClass('fixed mdl-shadow--4dp');
 				$('header a').css({
-					"font-size": "11px",
-					"padding": "40px 0px"
+					"font-size": "11px"
 				});
 				$('.logo a img').css({
 					"height": "40px",
@@ -311,8 +310,7 @@ $(document).ready(function() {
 				});
 			}else {
 				$('header a').css({
-					"font-size": "12px",
-					"padding": "45px 0px"
+					"font-size": "12px"
 				});
 				$('.logo a img').css({
 					"height": "50px",
