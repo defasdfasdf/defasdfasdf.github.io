131e11899b48d414ee5c735cad96113ec3f00e2e
ShieldCoin <ShieldCoin@protonmail.com>
2017-11-25 14:59:43 +0100

    Update smoothscroll.js

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -2,13 +2,10 @@ $("a[href*='#']")
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .not('[href*="#test4"]')
-  .not('[href*="#test5"]')
-  .not('[href*="#test6"]')
   .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+      //location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
