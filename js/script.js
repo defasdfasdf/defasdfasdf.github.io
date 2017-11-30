3098aec76b65d7821a6a20b9b26d77eedfb186e1
defasdfasdf <l2568588@mvrht.net>
2017-11-30 21:22:03 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -65,20 +65,31 @@ function title(){
 });
 }
 function wallets(){
+  var w =0;
+  var l = 0;
+  var a = 0;
+  var o = 0;
   information.forEach(function(element){
     if (element.platform.includes("windows")){
-      var x = "windowscontent";
+      var x = "w";
+      w++;
     }
     else if (element.platform.includes("linux")){
-      var x = "linuxcontent";
+      var x = "l";
+      l++;
     }
     else if (element.platform.includes("android")){
-      var x = "androidcontent";
+      var x = "a";
+      a++;
     }
     else if (element.platform.includes("other")){
-      var x = "othercontent";
+      var x = "o";
+      o++;
     }
       var wallet = document.createElement("div");
+      if (x == "w"){w = w %2; if(w = 0){wallet.classList.add("s1-offset");} }else if (x == "l"){l = l %2; if(l == 0){wallet.classList.add("s1-offset");}}
+      else if (x == "a"){a = a %2; if(a == 0){wallet.classList.add("s1-offset");}}else if (x == "o"){o = o % 2; if(o == 0){wallet.classList.add("s1-offset");}}
+      
       wallet.classList.add("col", "s12","m5","card");
       var typeh3 = document.createElement("div");
       typeh3.classList.add("card-content");
