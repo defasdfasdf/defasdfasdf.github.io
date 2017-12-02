a278e276c4bab23978fc457c29260baf9044bb7d
defasdfasdf <l2568588@mvrht.net>
2017-12-02 11:53:40 +0100

    Mmm

--- js/script.js
+++ js/script.js
@@ -167,7 +167,7 @@ function coinlists() {
             var at = document.createTextNode(element.Name);
             a.appendChild(at);
             div2.appendChild(a);
-          }););
+          }));
             bdiv.appendChild(div2);
             var div = document.createElement("div");
             div.classList.add(classs ,"exchange-card-image","mdl-cell","mdl-cell--3-col-desktop","mdl-cell--4-col","mdl-card","mdl-shadow--2dp","eximg");
