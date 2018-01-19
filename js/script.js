c34e8c3d8eff8bbe8c05b0d6361961238dfcc7ef
defasdfasdf <31629709+defasdfasdf@users.noreply.github.com>
2018-01-19 22:51:44 +0100

    Nice

--- js/script.js
+++ js/script.js
@@ -473,6 +473,7 @@ var yes = 0;
                     tension: 0,
                     label: "XSH line",
                     borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgb(255,255,255)',
                     data: XSHlineinfo
                 }]
             },
