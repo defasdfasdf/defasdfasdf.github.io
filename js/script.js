349a5f4bf9cbb2bc1799011901664f93fb0f5fcd
defasdfasdf <31629709+defasdfasdf@users.noreply.github.com>
2018-01-20 10:07:46 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -470,7 +470,7 @@ var yes = 0;
             data: {
               labels: XSHtllineinfo,
                 datasets: [{
-                    tension: .2,
+                    tension: .4,
                     borderColor: 'rgb(255, 255, 255)',
                     backgroundColor: 'rgba(255,255,255,0)',
                     data: XSHlineinfo
